4e14ac61c604654fb4d33c1b145deb5994843f07
Garrido <garri@mierdoshiba.(none)>
2018-05-26 14:29:57 +0200

    260518

---
+++ A/pizzaPizzaPizza.c
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long n;
+	cin >> n;
+	
+	if(n == 0)cout << 0 << endl;
+	else {
+		if(n%2 != 0) cout << n/2 + 1 << endl;
+		else cout << n+1 << endl;
+	}
+}
