49a5e49286580c656f3c8ce3aa9c80da9b72b44a
Marc <Garridocho@gmail.com>
2022-02-05 22:34:20 +0100

    Update roofConstruction.cpp

--- B/roofConstruction.cpp
+++ B/roofConstruction.cpp
@@ -18,16 +18,7 @@ void solve() {
 	int min = n;
 
         int k = 0;
-        while((1 << (k + 1)) <= n - 1) ++k; 
-        
-        for(int i = (1 << k) - 1; i >= 0; i--) {
-            cout << i << ' ';
-        }
-        for(int i = (1 << k); i < n; i++) {
-            cout << i << ' ';
-        }
-        cout << '\n';
-    
+       
 }
 
 int main() {
