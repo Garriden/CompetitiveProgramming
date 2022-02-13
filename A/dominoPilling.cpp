495c2ddfb9fd82b743811e29647d025f2c551cc7
Marc <Garridocho@gmail.com>
2022-02-13 13:50:14 +0100

    #greedy #math

---
+++ A/dominoPilling.cpp
@@ -0,0 +1,26 @@
+/*********************
+https://codeforces.com/problemset/problem/50/A
+A
+800
+
+greedy
+math
+*************************/
+
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+void solve() {
+    int n, m;
+	cin >> n >> m;
+
+    cout << n*m / 2 << endl;
+}
+
+int main() {
+	//int T;
+	//cin >> T;
+	//while (T--)
+		solve();
+}
