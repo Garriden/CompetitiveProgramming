4a69ed85ca161a168a21992586ec28c1744668f4
teteDiglett <Garridocho@gmail.com>
2025-01-25 00:58:31 +0100

    0702 Two Pointers.

--- Cracking/02LinkedLists/0702Intersection.cpp
+++ Cracking/02LinkedLists/0702Intersection.cpp
@@ -15,14 +15,59 @@
     - Iterate s2, check map.
     Time complexity: O(n)
 
-  - Two Pointers?
+  - Two Pointers
     - Run each list, get the lenghts and tails.
-    - Compare the tails, if they're the same, there is an intersection.
     - On the longer list, start on his length - smaller list lenght.
     - Iterate and compare.
     Time complexity: O(n)
     Space complexity: O(1)
 */
+
+Node* IntersectTwoPointers(Node* &n1, Node* &n2)
+{
+    // Run each list, get the lenghts and tails.
+    Node *n1Tail = n1;
+    Node *n2Tail = n2;
+    int n1length = 0;
+    int n2length = 0;
+
+    while(n1Tail != nullptr) {
+        n1Tail = n1Tail->next;
+        ++n1length;
+    }
+
+    while(n2Tail != nullptr) {
+        n2Tail = n2Tail->next;
+        ++n2length;
+    }
+
+    // On the longer list, start on his length - smaller list lenght.
+    int size = n2length - n1length;
+    n1Tail = n1;
+    n2Tail = n2;
+    for(int ii = 0; ii < size; ++ii) {
+        n2Tail = n2Tail->next;
+    }
+
+    if(n1length > n2length) {
+        size = n1length - n2length;
+        for(int ii = 0; ii < size; ++ii) {
+            n1Tail = n1Tail->next;
+        }
+        n2Tail = n2;
+    }
+
+    // Iterate and compare.
+    while(n1Tail != nullptr) {
+        if(n1Tail == n2Tail) {
+            return n1Tail;
+        }
+        n2Tail = n2Tail->next;
+        n1Tail = n1Tail->next;
+    }
+
+    return nullptr;
+}
 
 Node* IntersectDS(Node* &n1, Node* &n2)
 {
@@ -64,7 +109,7 @@ int main()
     n2->next->next = n->next->next->next;
     LinkedList::PrintList(n2);
 
-    std::cout << IntersectDS(n, n2)->data << std::endl;
+    std::cout << IntersectTwoPointers(n, n2)->data << std::endl;
 
     return 0;
 }
