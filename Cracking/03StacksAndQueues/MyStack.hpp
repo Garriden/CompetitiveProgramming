80cd270eaf5b89bf384839b37d451e6920375693
teteDiglett <Garridocho@gmail.com>
2025-01-27 23:53:16 +0100

    Strarting stacks.

---
+++ Cracking/03StacksAndQueues/MyStack.hpp
@@ -0,0 +1,19 @@
+#ifndef MY_STACK_H
+#define MY_STACK_H
+
+template <typename T>
+class MyStack 
+{
+    public:
+    
+
+    private:
+    struct Node {
+        T value;
+        Node *next;
+    }
+
+}
+
+
+#endif
