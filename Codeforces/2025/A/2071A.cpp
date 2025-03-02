0af0fe8e3a72f88525b89adb387251b86b54193d
teteDiglett <Garridocho@gmail.com>
2025-03-02 19:11:11 +0100

    Codeforces 0103 A.

---
+++ Codeforces/2025/A/2071A.cpp
@@ -0,0 +1,30 @@
+//https://codeforces.com/contest/2071/problem/A
+
+// g++ -o hola a.cpp
+//Just check.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>       /* pow */
+#define ll unsigned long long
+using namespace std;
+
+int main() {
+    ll t, n, k, CA;
+    ll a;
+    std::cin >> t;
+    while(t--) {
+        ll sum = 0;
+        std::cin >> k;
+        ll N = n;
+        ll max = 0;
+        if((k % 3) == 0 ) {
+            std::cout << "NO" << std::endl;
+        } else {
+            std::cout << "YES" << std::endl;
+        }
+        //std::cout << sum << std::endl;
+    }
+
+}
