6f05b403e309bd69a99e3c9b949c8d2d4e48ee86
Marc <Garridocho@gmail.com>
2025-05-24 19:48:40 +0200

    Codeforces round 1026

---
+++ Main.cpp
@@ -0,0 +1,117 @@
+
+
+// Jj
+
+// g++ -o b b.cpp
+
+// game
+// brute force
+// greedy
+// implementation
+
+// If it' even or odd and count two pointers.
+
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+
+int main() {
+    ll t, n, m, x;
+    ll a, b, p;
+    std::string s;
+    std::cin >> t;
+
+    while(t --> 0) {
+        std::cin >> n;
+        ll sum = 0;
+        ll firstPointer = 0;
+        ll lastPointer = n - 1;
+
+        std::vector<ll> va(n);
+        
+        
+        for(int ii = 0; ii < n; ++ii) {
+            std::cin >> a;
+            va[ii] = a;
+        }
+
+        std::sort(va.begin(), va.end());
+
+
+        
+        
+        
+
+
+ll firstDistance = 0;
+ll lastDistance = 0;
+bool firsteven = true;
+if(va[0] % 2 == 1){ //0dd
+  firsteven=false;
+}
+
+for(int ii = 1; ii < n; ++ii) {
+  if(firsteven) {
+    if(va[ii]% 2 == 0) { //nothi
+    
+    } else { // ojo
+firstDistance = ii;
+break;
+}
+} else { //odd
+if(va[ii]% 2 == 0) {
+     firstDistance = ii;
+     break;
+    }else {
+  
+}
+
+}
+}
+
+
+bool lasteven = true;
+if(va[lastPointer] % 2 == 1){ //0dd
+  lasteven=false;
+}
+
+for(int ii = n-2; ii >=0; --ii) {
+  if(lasteven) {
+    if(va[ii]% 2 == 0) { //nothi
+     
+    } else { // ojo
+lastDistance = n-ii-1;
+break;
+
+}
+} else { //odd
+if(va[ii]% 2 == 0) {
+     lastDistance = n-ii-1;
+     break;
+    } else {
+      
+    }
+    }
+}
+
+
+
+
+
+//std::cout << "firstDistance: " <<firstDistance << "lastd  " << lastDistance << std::endl;
+
+if(firsteven == lasteven){
+std::cout <<0 << std::endl;
+
+} else {
+        std::cout << std::min(firstDistance, lastDistance) << std::endl;
+}
+        
+
+
+
+
+    }
+
+}
